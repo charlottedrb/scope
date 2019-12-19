7d690ebedcf65cca0f5e2469094416103b410ba4
maximegi <maxime.giraudeau1@gmail.com>
2019-12-19 10:05:37 +0100

    tg

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const desc = document.getElementById('description');
+const post = document.getElementById('poster');
+const date = document.getElementById('date');
+const duree = document.getElementById('duree');
+const act = document.getElementById('acteurs');
+const recomp = document.getElementById('recompenses');
+const box = document.getElementById('boxoffice');
+const real = document.getElementById('real');
+const genre = document.getElementById('genre');
+const ecrit = document.getElementById('ecriture');
+
+
 function search() {
     var search = document.getElementById('search').value;
     var text = 'http://www.omdbapi.com/?t=' + search + '&apikey=4a591bbb';
@@ -6,13 +18,19 @@ function search() {
             response.json() //analyse la réponse en JSON
                 .then(function (value) {
                     console.log(value);
-                    document.getElementById('description').innerHTML = value.Plot;
-                    document.getElementById('poster').src = value.Poster;
+                    desc.innerHTML = 'Story : ' + value.Plot;
+                    post.src = value.Poster;
+                    date.innerHTML = 'Date : ' + value.Released;
+                    duree.innerHTML = 'Runtime : ' + value.Runtime;
+                    act.innerHTML = 'Actors : ' + value.Actors;
+                    recomp.innerHTML = 'Awards : ' + value.Awards;
+                    box.innerHTML = 'BoxOffice : ' + value.BoxOffice;
+                    real.innerHTML = 'Director : ' + value.Director;
+                    genre.innerHTML = 'Genre : ' + value.Genre;
+                    ecrit.innerHTML = 'Writer : ' + value.Writer;
                 });
         });
 }
 
 //function search(){
 //}
-
->>>>>>> be7237e82b38d46dc59d40ed4b960d89ef5c2fe5
