1c9a3b87548549b8901c40cf384597950cc8f1e8
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2019-12-19 09:11:59 +0100

    init projet film

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const request = fetch('http://www.omdbapi.com/?t=avengers&apikey=4a591bbb')
-.then(function (response) {
-    response.json() //analyse la réponse en JSON
-        .then(function (value) {
-            console.log(value);
-            document.getElementById('description').innerHTML = value.Plot;
-            
+function search() {
+    var search = document.getElementById('search').value;
+    var text = 'http://www.omdbapi.com/?t=' + search + '&apikey=4a591bbb';
+    const request = fetch(text)
+        .then(function (response) {
+            response.json() //analyse la réponse en JSON
+                .then(function (value) {
+                    console.log(value);
+                    document.getElementById('description').innerHTML = value.Plot;
+                    document.getElementById('poster').src = value.Poster;
+                });
         });
-    });
+}
 
-
-
-
+//function search(){
+//}
 
