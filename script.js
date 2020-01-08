96244749d1f6a649068dbdae51750229e1c9cc45
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2020-01-08 23:46:30 +0100

    finishhh

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//function search(){
-//}
+// On récupère tous nos éléments HTML sur lesquels on a placé des id pour ensuite y placer des données
 const movie = document.getElementById('movie');
 const desc = document.getElementById('description');
 const post = document.getElementById('poster');
@@ -12,19 +11,27 @@ const real = document.getElementById('real');
 const genre = document.getElementById('genre');
 const ecrit = document.getElementById('ecriture');
 const title = document.getElementById('movieTitle');
+const ytplayer = document.getElementById('ytplayer');
+const button = document.getElementById('letsgo');
 
+// On cache la div de classe "movie" pour ne pas que l'espace qu'elle prend soit "présent" sur le site même si celle-ci est vide
 movie.style.display = "none";
+button.addEventListener("click", function (e) {
+    e.preventDefault();
+    return false;
+});
 
 function search(){
+    // Une fois qu'on a lancé la recherche on peut affiche la div de classe "movie"
     movie.style.display = "flex";
-    var search = document.getElementById('search').value;
+    var search = document.getElementById('recherche').value;
     var text = 'http://www.omdbapi.com/?t=' + search + '&apikey=4a591bbb';
-    const request = fetch(text)
-        .then(function (response) {
+    const request = fetch(text) //appel de l'API
+        .then(function (response) { //récupération de la réponse de l'API en JSON
             response.json() //analyse la réponse en JSON
-                .then(function (value) {
-                    console.log(value);
-                    // document.location.href="movie.html";
+                .then(function (value) { //récupartion de la valeur de la réponse
+                    console.log(value); 
+                    // On met la valeur reçue dans le conteneur qui lui convient grâce aux id récupéré plus haut
                     desc.innerHTML = "<b>Story </b>" + ": " + value.Plot;
                     post.src = value.Poster;
                     date.innerHTML = "<b>Date </b>" + ": " + value.Released;
@@ -38,16 +45,7 @@ function search(){
                     title.innerHTML = value.Title.toUpperCase();
                 });
         });
-
-    ytplayer = document.getElementById('ytplayer');
-    ytplayer.src = "https://www.youtube.com/embed/?listType=search&list=" + search + "trailer";
-    // var link = 'https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=' + search + '%20bande%20annoncetype=video&videoDefinition=high&key=AIzaSyA8sfoWgWYvCeozwyxf5ZSPV839_XDp5-Q';
-    // const YTrequest = fetch(link)
-    //     .then(function (response) {
-    //         response.json()
-    //             .then(function (value) {
-    //                 console.log(value);
-
-    //             });
-    //     });
+    // Permet d'afficher la bande-annonce
+    ytplayer.src = "https://www.youtube.com/embed/?listType=search&list=" + search + "trailer"; // on utilise une URL trouvée sur la doc d'API Youtube  à laquelle on 
+                                                                                                // rajoute notre titre de film et le mot "trailer" pour affiche la BA
 }
